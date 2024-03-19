1201b646c3ed7099c85ff516acc72bd4a543fd18
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-03-19 19:56:11 +0700

    develop

--- src/voucher/voucher.service.ts
+++ src/voucher/voucher.service.ts
@@ -28,16 +28,20 @@ export class VoucherService {
     }
 
     async createVoucher({ dto }: { dto: CreateVoucherDto[] }) {
-        await this.prisma.voucher.createMany({
-            data: dto,
-        });
+        await this.prisma.voucher.createMany({ data: dto });
     }
 
     async updateVoucher({ id, dto }: { id: string; dto: UpdateVoucherDto }) {
         const existVoucher = await this.getVoucherById({ id: id });
         return await this.prisma.voucher.update({
             where: { id: existVoucher.id },
-            data: { ...dto },
+            data: {
+                categoryId: dto.categoryId ? dto.categoryId : existVoucher.categoryId,
+                code: dto.code ? dto.code : existVoucher.code,
+                link: dto.link ? dto.link : existVoucher.link,
+                price: dto.price ? dto.price : existVoucher.price,
+                status: dto.status ? dto.status : existVoucher.status,
+            },
         });
     }
 
@@ -48,46 +52,22 @@ export class VoucherService {
 
     async receiveVoucher({ dto }: { dto: ReceiveVoucherDto }) {
         const account = await this.prisma.account.findFirst({
-            where: {
-                walletAddress: dto.walletAddress,
-                stakeKey: dto.stakeAddress,
-            },
+            where: { walletAddress: dto.walletAddress, stakeKey: dto.stakeAddress },
         });
         const existVouchers = await this.prisma.accountVoucher.findMany({
-            where: {
-                accountId: account.id,
-            },
-            include: {
-                voucher: true,
-            },
+            where: { accountId: account.id },
+            include: { voucher: true },
         });
-
-        if (existVouchers.length > 0) {
-            return existVouchers;
-        }
-
+        if (existVouchers.length > 0) return existVouchers;
         let price: string;
-        if (dto.epoch > 0 && dto.epoch <= 1) {
-            price = "100";
-        } else if (dto.epoch > 1 && dto.epoch <= 2) {
-            price = "200";
-        } else if (dto.epoch > 2) {
-            price = "300";
-        }
-
+        if (dto.epoch > 0 && dto.epoch <= 1) price = "100";
+        else if (dto.epoch > 1 && dto.epoch <= 2) price = "200";
+        else if (dto.epoch > 2) price = "300";
         const freeVoucher = await this.prisma.voucher.findFirst({
-            where: {
-                status: "FREE",
-                categoryId: dto.categoryId,
-                price: price,
-            },
+            where: { status: "FREE", categoryId: dto.categoryId, price: price },
         });
-
         await this.prisma.accountVoucher.create({
-            data: {
-                accountId: account.id,
-                voucherId: freeVoucher.id,
-            },
+            data: { accountId: account.id, voucherId: freeVoucher.id },
         });
         return [freeVoucher];
     }
